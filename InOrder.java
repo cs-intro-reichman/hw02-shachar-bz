87dc966cb58dccf77bb08158c54bcce9b4bcf853
shachar-bz <shachar.benzur@runi.ac.il>
2023-12-27 14:38:45 +0200

    mycode

--- InOrder.java
+++ InOrder.java
@@ -5,30 +5,22 @@
 public class InOrder {
 	public static void main (String[] args) {
 		int num1 = (int) (Math.random() * 10);
-		//convert int to string
-		String answer = Integer.toString(num1);     
+		// print first number
+		System.out.print(num1 + " ");
 
-		// check if there will be another numbers in the program
-		int random = (int) (Math.random() * 10);
 		boolean non_decreasing = false;
-
-		int num2 = (int) (Math.random() * 10);
 		
 		while (non_decreasing == false){
-			if (num2 == num1 - 1){
-				non_decreasing = true;
-			}
-			else {
-				// add number that meet the conditions
-				answer += " " + num2;
+			// compare between two number
+			int num2 = (int) (Math.random() * 10);
+			if(num1 <= num2){
+				System.out.print(num2 + " ");
 				num1 = num2;
 			}
-			// create another number between 0-10
-			num2 = (int) (Math.random() * 10);
-			// random a number and check if the while loop will continue to work
-			random = (int) (Math.random() * 10); 			
+			else{
+				non_decreasing = true;
+			}
+			
 		}
-
-		System.out.println(answer);
 	}
 }
